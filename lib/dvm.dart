2fe90b3d9a63b2153060caa47304ad8d8b9fce3a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-05 19:42:52 +0900

    feat: Implement an extension to flush then exit

--- lib/dvm.dart
+++ lib/dvm.dart
@@ -1,3 +1,18 @@
+import 'dart:io';
+
+import 'package:dvm/src/data/exit_status.dart';
+
 int calculate() {
   return 6 * 7;
+}
+
+extension FlushThenExit on ExitStatus {
+  Future<void> flushThenExit() async => Future.wait(
+        [
+          stdout.close(),
+          stderr.close(),
+        ],
+      ).then(
+        (_) => exit(code),
+      );
 }
