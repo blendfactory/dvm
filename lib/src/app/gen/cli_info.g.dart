22e116be76db70ee01aa228308c714c49f6432a8
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-07 21:41:10 +0900

    feat: Add CliInfo class and cliInfo constant

---
+++ lib/src/app/gen/cli_info.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:dvm/src/app/models/cli_info.dart';
+
+const cliInfo = CliInfo(
+  name: 'dvm',
+  description: 'A simple CLI to manage Dart SDK versions.',
+  version: '0.0.1',
+);
---
+++ lib/src/app/models/cli_info.dart
@@ -0,0 +1,11 @@
+final class CliInfo {
+  const CliInfo({
+    required this.name,
+    required this.description,
+    required this.version,
+  });
+
+  final String name;
+  final String description;
+  final String version;
+}
