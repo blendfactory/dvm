cbf18c816dc64f14c902b83cfdf226ebf9b48c51
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-07 23:09:19 +0900

    feat: Create ReleasesCommandService class

---
+++ lib/src/app/command_services/releases_command_services.dart
@@ -0,0 +1,51 @@
+import 'package:dvm/src/app/models/exit_status.dart';
+import 'package:dvm/src/app/servicies/console_service.dart';
+import 'package:dvm/src/features/sdk/models/sdk_channel.dart';
+import 'package:dvm/src/features/sdk/services/sdk_service.dart';
+import 'package:http/http.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'releases_command_services.g.dart';
+
+@Riverpod(
+  dependencies: [
+    sdkService,
+    consoleService,
+  ],
+)
+ReleasesCommandService releasesCommandService(
+  ReleasesCommandServiceRef ref,
+) {
+  final sdkService = ref.watch(sdkServiceProvider);
+  final consoleService = ref.watch(consoleServiceProvider);
+  return ReleasesCommandService(
+    sdkService: sdkService,
+    consoleService: consoleService,
+  );
+}
+
+final class ReleasesCommandService {
+  ReleasesCommandService({
+    required SdkService sdkService,
+    required ConsoleService consoleService,
+  })  : _sdkService = sdkService,
+        _consoleService = consoleService;
+
+  final SdkService _sdkService;
+  final ConsoleService _consoleService;
+
+  Future<ExitStatus> call({
+    required SdkChannel channel,
+  }) async {
+    try {
+      final versions = await _sdkService.getSdkVersions(channel: channel);
+      for (final version in versions) {
+        _consoleService.info(version.toString());
+      }
+      return ExitStatus.success;
+    } on ClientException catch (e) {
+      _consoleService.error(e.toString());
+      return ExitStatus.error;
+    }
+  }
+}
---
+++ lib/src/app/command_services/releases_command_services.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'releases_command_services.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$releasesCommandServiceHash() =>
+    r'cee64ade05f59b606ffb1de630e7fe732b1c5db6';
+
+/// See also [releasesCommandService].
+@ProviderFor(releasesCommandService)
+final releasesCommandServiceProvider =
+    AutoDisposeProvider<ReleasesCommandService>.internal(
+  releasesCommandService,
+  name: r'releasesCommandServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$releasesCommandServiceHash,
+  dependencies: <ProviderOrFamily>[sdkServiceProvider, consoleServiceProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    sdkServiceProvider,
+    ...?sdkServiceProvider.allTransitiveDependencies,
+    consoleServiceProvider,
+    ...?consoleServiceProvider.allTransitiveDependencies
+  },
+);
+
+typedef ReleasesCommandServiceRef
+    = AutoDisposeProviderRef<ReleasesCommandService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
