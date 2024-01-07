75da7ae15e4d4aafa3e8700d2c21bbb74f96f618
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-08 00:11:13 +0900

    feat: Add warning and spacer methods to ConsoleService

--- lib/src/app/servicies/console_service.dart
+++ lib/src/app/servicies/console_service.dart
@@ -12,6 +12,8 @@ final class ConsoleService {
 
   void info(String message) => print(message);
 
+  void warning(String message) => print('\x1B[33m$message\x1B[0m');
+
   void error(String message) {
     final msg = '''
 An unexpected error occurred.
@@ -20,4 +22,6 @@ Consider creating an issue on https://github.com/blendfactory/dvm/issues/new.
 $message''';
     print('\x1B[31m$msg\x1B[0m');
   }
+
+  void spacer() => print('');
 }
