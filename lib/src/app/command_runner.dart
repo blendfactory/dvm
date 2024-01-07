f156119bf0ed3908936acc24a34941baeec2dabe
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-08 00:12:38 +0900

    improve: Change to print exception message

--- lib/src/app/command_runner.dart
+++ lib/src/app/command_runner.dart
@@ -3,10 +3,12 @@ import 'package:args/command_runner.dart';
 import 'package:dvm/src/app/commands/releases_command.dart';
 import 'package:dvm/src/app/gen/cli_info.g.dart';
 import 'package:dvm/src/app/models/exit_status.dart';
+import 'package:dvm/src/app/servicies/console_service.dart';
 
 final class DvmCommandRunner extends CommandRunner<ExitStatus> {
-  DvmCommandRunner()
-      : super(
+  DvmCommandRunner([
+    this._consoleService = const ConsoleService(),
+  ]) : super(
           cliInfo.name,
           cliInfo.description,
         ) {
@@ -19,6 +21,8 @@ final class DvmCommandRunner extends CommandRunner<ExitStatus> {
     addCommand(ReleasesCommand());
   }
 
+  final ConsoleService _consoleService;
+
   @override
   Future<ExitStatus> run(Iterable<String> args) async {
     try {
@@ -28,11 +32,27 @@ final class DvmCommandRunner extends CommandRunner<ExitStatus> {
         return ExitStatus.success;
       }
       return await runCommand(argResults) ?? ExitStatus.success;
-    } on UsageException catch (_) {
+    } on UsageException catch (e) {
+      _printUsageException(e);
       return ExitStatus.usage;
-    } on Exception catch (_) {
+    } on Exception catch (e) {
+      _printException(e);
       return ExitStatus.error;
     }
+  }
+
+  void _printUsageException(UsageException e) {
+    _consoleService.spacer();
+    _consoleService.warning(e.message);
+    _consoleService.spacer();
+    _consoleService.info(e.usage);
+    _consoleService.spacer();
+  }
+
+  void _printException(Exception e) {
+    _consoleService.spacer();
+    _consoleService.error(e.toString());
+    _consoleService.spacer();
   }
 }
 
