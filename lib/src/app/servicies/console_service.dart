18fcd8c5968b0532f4e1af18bbefad8b48765fe7
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-07 23:08:38 +0900

    feat: Add error() to ConsoleService

--- lib/src/app/servicies/console_service.dart
+++ lib/src/app/servicies/console_service.dart
@@ -11,4 +11,13 @@ final class ConsoleService {
   const ConsoleService();
 
   void info(String message) => print(message);
+
+  void error(String message) {
+    final msg = '''
+An unexpected error occurred.
+Consider creating an issue on https://github.com/blendfactory/dvm/issues/new.
+
+$message''';
+    print('\x1B[31m$msg\x1B[0m');
+  }
 }
