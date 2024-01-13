57636ebb1395819086d1df3a86b730eda02dd5a4
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-13 21:31:18 +0900

    feat: Add DartService class for running Dart commands

---
+++ lib/src/app/servicies/dart_service.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:dvm/src/cores/local/sdk_cache_dir.dart';
+import 'package:dvm/src/features/sdk/models/sdk_version.dart';
+import 'package:file/file.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dart_service.g.dart';
+
+@Riverpod(
+  dependencies: [
+    sdkCacheDir,
+  ],
+)
+DartService dartService(
+  DartServiceRef ref,
+) {
+  final sdkCacheDir = ref.watch(sdkCacheDirProvider);
+  return DartService(
+    sdkCacheDir: sdkCacheDir,
+  );
+}
+
+final class DartService {
+  DartService({
+    required Directory sdkCacheDir,
+  }) : _sdkCacheDir = sdkCacheDir;
+
+  final Directory _sdkCacheDir;
+
+  Future<ProcessResult> runWithVersion({
+    required SdkVersion version,
+    required List<String> args,
+  }) async {
+    final versionCacheDir = _sdkCacheDir.childDirectory(version.toString());
+
+    final dartBinDir = versionCacheDir.childDirectory('bin');
+    final dartExecutableFileName = Platform.isWindows ? 'dart.bat' : 'dart';
+    final dartExecutableFile = dartBinDir.childFile(dartExecutableFileName);
+    if (!dartExecutableFile.existsSync()) {
+      throw Exception('Not found dart executable file.');
+    }
+
+    return Process.run(
+      dartExecutableFile.path,
+      args,
+    );
+  }
+}
---
+++ lib/src/app/servicies/dart_service.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'dart_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dartServiceHash() => r'a2acd0fa9bb00f4bd32285c9087753e0c716d9ad';
+
+/// See also [dartService].
+@ProviderFor(dartService)
+final dartServiceProvider = AutoDisposeProvider<DartService>.internal(
+  dartService,
+  name: r'dartServiceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$dartServiceHash,
+  dependencies: <ProviderOrFamily>[sdkCacheDirProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    sdkCacheDirProvider,
+    ...?sdkCacheDirProvider.allTransitiveDependencies
+  },
+);
+
+typedef DartServiceRef = AutoDisposeProviderRef<DartService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
