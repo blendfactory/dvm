8500564749cb6ae3c14b2f553a9bf01d8bc038a9
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-14 22:11:45 +0900

    fix: Update GitHub user and repository information

--- tool/grind.dart
+++ tool/grind.dart
@@ -2,8 +2,13 @@ import 'package:cli_pkg/cli_pkg.dart' as pkg;
 import 'package:dvmx/src/app/gen/cli_info.g.dart';
 import 'package:grinder/grinder.dart';
 
+const _githubUser = 'blendthink';
+const _githubRepo = 'blendfactory/dvm';
+
 Future<void> main(List<String> args) async {
   pkg.name.value = cliInfo.name;
+  pkg.githubUser.value = _githubUser;
+  pkg.githubRepo.value = _githubRepo;
 
   pkg.addGithubTasks();
 
