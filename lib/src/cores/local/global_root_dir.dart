538d99a29cee92f4cc2d68aa8c1cb853ffd8ac56
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-03-14 00:20:28 +0900

    feat: Implement a mechanism to provide a global root directory

---
+++ lib/src/cores/local/global_root_dir.dart
@@ -0,0 +1,26 @@
+import 'package:dvmx/src/cores/local/development_dir.dart';
+import 'package:dvmx/src/cores/local/dvm_home_dir.dart';
+import 'package:dvmx/src/cores/local/is_debug.dart';
+import 'package:file/file.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'global_root_dir.g.dart';
+
+@Riverpod(
+  dependencies: [
+    isDebug,
+    developmentDir,
+    dvmHomeDir,
+  ],
+)
+Directory globalRootDir(GlobalRootDirRef ref) {
+  final isDebug = ref.watch(isDebugProvider);
+  final developmentDir = ref.watch(developmentDirProvider);
+  final dvmHomeDir = ref.watch(dvmHomeDirProvider);
+
+  if (isDebug) {
+    return developmentDir.childDirectory('global');
+  } else {
+    return dvmHomeDir;
+  }
+}
