6f16fee69b524b3ddc6223d1d4f78b9b6cb3ef21
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-03-17 19:31:16 +0900

    feat: support global config for Dart commands

--- lib/src/app/command_services/dart_command_service.dart
+++ lib/src/app/command_services/dart_command_service.dart
@@ -1,6 +1,8 @@
 import 'package:dvmx/src/app/models/exit_status.dart';
 import 'package:dvmx/src/app/servicies/console_service.dart';
 import 'package:dvmx/src/app/servicies/dart_service.dart';
+import 'package:dvmx/src/cores/models/sdk_version.dart';
+import 'package:dvmx/src/features/global_config/services/global_config_service.dart';
 import 'package:dvmx/src/features/project_config/services/project_config_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -9,6 +11,7 @@ part 'dart_command_service.g.dart';
 @Riverpod(
   dependencies: [
     projectConfigService,
+    globalConfigService,
     dartService,
     consoleService,
   ],
@@ -17,10 +20,12 @@ DartCommandService dartCommandService(
   DartCommandServiceRef ref,
 ) {
   final projectConfigService = ref.watch(projectConfigServiceProvider);
+  final globalConfigService = ref.watch(globalConfigServiceProvider);
   final dartService = ref.watch(dartServiceProvider);
   final consoleService = ref.watch(consoleServiceProvider);
   return DartCommandService(
     projectConfigService: projectConfigService,
+    globalConfigService: globalConfigService,
     dartService: dartService,
     consoleService: consoleService,
   );
@@ -29,13 +34,16 @@ DartCommandService dartCommandService(
 final class DartCommandService {
   DartCommandService({
     required ProjectConfigService projectConfigService,
+    required GlobalConfigService globalConfigService,
     required DartService dartService,
     required ConsoleService consoleService,
   })  : _projectConfigService = projectConfigService,
+        _globalConfigService = globalConfigService,
         _dartService = dartService,
         _consoleService = consoleService;
 
   final ProjectConfigService _projectConfigService;
+  final GlobalConfigService _globalConfigService;
   final DartService _dartService;
   final ConsoleService _consoleService;
 
@@ -49,10 +57,17 @@ final class DartCommandService {
       );
     }
 
-    final sdkVersion = _projectConfigService.findProjectSdkVersion();
+    final SdkVersion? sdkVersion;
+    final projectSdkVersion = _projectConfigService.findProjectSdkVersion();
+    if (projectSdkVersion == null) {
+      sdkVersion = _globalConfigService.findGlobalSdkVersion();
+    } else {
+      sdkVersion = projectSdkVersion;
+    }
+
     if (sdkVersion == null) {
       throwUsageException(
-        'Please set a version in project config.',
+        'Please set a version in project or global config.',
       );
     }
 
