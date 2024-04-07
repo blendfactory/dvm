20d78012daadb73c23b61ee2cd37441779d62c0f
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-04-07 18:02:05 +0900

    feat: implement status to CheckResult conversion

--- lib/src/app/command_services/doctor_command_service.dart
+++ lib/src/app/command_services/doctor_command_service.dart
@@ -1,7 +1,11 @@
+import 'package:dvmx/src/app/models/check_status.dart';
 import 'package:dvmx/src/app/models/exit_status.dart';
 import 'package:dvmx/src/app/servicies/console_service.dart';
+import 'package:dvmx/src/features/global_config/models/global_config_status.dart';
 import 'package:dvmx/src/features/global_config/services/global_config_service.dart';
+import 'package:dvmx/src/features/package_version/models/package_version_status.dart';
 import 'package:dvmx/src/features/package_version/services/package_version_service.dart';
+import 'package:dvmx/src/features/project_config/models/project_config_status.dart';
 import 'package:dvmx/src/features/project_config/services/project_config_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -55,5 +59,103 @@ final class DoctorCommandService {
     progress.complete('Check completed.');
 
     return ExitStatus.success;
+  }
+}
+
+extension on PackageVersionStatus {
+  CheckResult toCheckResult() {
+    return switch (this) {
+      PackageVersionStatusUpToDate() => (
+          status: CheckStatus.success,
+          summary: 'Your CLI is up to date. No update is required.',
+          detail: null,
+        ),
+      PackageVersionStatusOutdated(current: final c, latest: final l) => (
+          status: CheckStatus.warning,
+          summary: 'Your CLI is outdated. Current: $c, Latest: $l.',
+          detail: null,
+        ),
+      final PackageVersionStatusError error => () {
+          final summary = switch (error) {
+            PackageVersionStatusAheadOfLatestError() =>
+              'Your CLI is ahead of the latest package version.',
+            PackageVersionStatusNetworkError() =>
+              'Failed to fetch the latest package version.',
+            PackageVersionStatusFormatError() =>
+              'Failed to format the data for the latest package version.',
+            PackageVersionStatusUnknownError() =>
+              '''An unexpected error occurred while fetching the latest package version.''',
+          };
+          return (
+            status: CheckStatus.error,
+            summary: summary,
+            detail: error.e?.toString(),
+          );
+        }(),
+    };
+  }
+}
+
+extension on GlobalConfigStatus {
+  CheckResult toCheckResult() {
+    return switch (this) {
+      GlobalConfigStatusFound(globalConfig: final config) => (
+          status: CheckStatus.success,
+          summary: 'Global config found.',
+          detail: config.toString(),
+        ),
+      GlobalConfigStatusNotFound() => (
+          status: CheckStatus.success,
+          summary: 'Global config not found.',
+          detail: null,
+        ),
+      final GlobalConfigStatusError error => () {
+          final summary = switch (error) {
+            GlobalConfigStatusJsonFormatError() =>
+              'Invalid JSON format in the global config file.',
+            GlobalConfigStatusParamFormatError() =>
+              'Invalid parameter format in the global config file.',
+            GlobalConfigStatusUnknownError() =>
+              '''An unexpected error occurred while reading the global config file.''',
+          };
+          return (
+            status: CheckStatus.error,
+            summary: summary,
+            detail: error.e?.toString(),
+          );
+        }(),
+    };
+  }
+}
+
+extension on ProjectConfigStatus {
+  CheckResult toCheckResult() {
+    return switch (this) {
+      ProjectConfigStatusFound(projectConfig: final config) => (
+          status: CheckStatus.success,
+          summary: 'Project config found.',
+          detail: config.toString(),
+        ),
+      ProjectConfigStatusNotFound() => (
+          status: CheckStatus.success,
+          summary: 'Project config not found.',
+          detail: null,
+        ),
+      final ProjectConfigStatusError error => () {
+          final summary = switch (error) {
+            ProjectConfigStatusJsonFormatError() =>
+              'Invalid JSON format in the project config file.',
+            ProjectConfigStatusParamFormatError() =>
+              'Invalid parameter format in the project config file.',
+            ProjectConfigStatusUnknownError() =>
+              '''An unexpected error occurred while reading the project config file.''',
+          };
+          return (
+            status: CheckStatus.error,
+            summary: summary,
+            detail: error.e?.toString(),
+          );
+        }(),
+    };
   }
 }
