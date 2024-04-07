302da3f280955414be01d5c1f14b2d3feb90932b
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-04-07 15:43:39 +0900

    feat: enhance console service with check results
    - Extend `ConsoleService` to include a `checkResult` method, allowing for detailed reporting of check outcomes based on `CheckStatus`.
    - Utilize color-coded tags to visually distinguish between success, warning, and error statuses in console outputs.
    - Implement conditional detail printing for comprehensive feedback on check results.

--- lib/src/app/servicies/console_service.dart
+++ lib/src/app/servicies/console_service.dart
@@ -1,3 +1,4 @@
+import 'package:dvmx/src/app/models/check_status.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -64,4 +65,27 @@ $message''';
         message,
         choices: choices,
       );
+
+  void checkResult(CheckResult result) {
+    final tag = switch (result.status) {
+      CheckStatus.success => lightGreen.wrap('[✓]'),
+      CheckStatus.warning => lightYellow.wrap('[!]'),
+      CheckStatus.error => lightRed.wrap('[✗]'),
+    };
+    _logger.info('$tag ${result.summary}');
+
+    if (result.detail case final String detail) {
+      final detailLines = detail.split('\n');
+      const indent = '    ';
+      for (final detailLine in detailLines) {
+        _logger.detail('$indent$detailLine');
+      }
+    }
+  }
 }
+
+typedef CheckResult = ({
+  CheckStatus status,
+  String summary,
+  String? detail,
+});
